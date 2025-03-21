6fc382ca0d08fb629181064d76bcbdc9d3638e42
Tahmauri <tmbo718@gmail.com>
2025-03-21 00:53:59 -0400

    started coding the calculation of N

--- Project_1.s
+++ Project_1.s
@@ -7,3 +7,9 @@ NAoutput: .asciiz "N/A"     #Output for invalid string
 
 main:
 
+    li $t0, 3134060 #loads Howrad ID into $t0
+    li $t1, 11 #loads 11 into $t1
+    div $t0, $t1 #divides id by 11
+    mfhi $t2 #stores remainder into $t2
+
+    li $t3, 26 #stores 26 into $t3
